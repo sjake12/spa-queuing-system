4ea9f796a2669290f570b809182bc75e2ee2e30e
sjake12 <apostol.sjake12@gmail.com>
2024-11-07 20:20:04 +0800

    Project, user and student info relation setup

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Student;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+
+        Student::factory()->create([
+            'student_id' => '28196',
+            'first_name' => 'Lee Robin',
+            'last_name' => 'Montenegro',
+            'course' => 'Computer Science',
+        ]);
+
+        User::create([
+            'username' => '28196',
+            'password' => Hash::make('1234'),
+        ]);
+
+        Student::factory()->create([
+            'student_id' => '28197',
+            'first_name' => 'Ralph Vincent',
+            'last_name' => 'Rodriguez',
+            'course' => 'Computer Science',
+        ]);
+
+        User::create([
+            'username' => '28197',
+            'password' => Hash::make('1234'),
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return redirect('/login');
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard', [
+        'auth' => [
+            'user' => auth()->user()->student,
+        ]
+    ]);
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
